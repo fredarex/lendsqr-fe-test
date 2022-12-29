84ebdad13ce88aed32a2508b3202f13721808e75
fredarex <fredobi200@gmail.com>
2022-12-29 13:04:11 +0100

    starting the project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <div>hello</div>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
