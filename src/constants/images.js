99f1bfea010b6447027307a4d8d8c687d8ab4163
fredarex <fredobi200@gmail.com>
2022-12-30 05:30:04 +0100

    login page done

---
+++ src/constants/images.js
@@ -0,0 +1,8 @@
+import login from '../assets/images/login.svg';
+import logo from '../assets/images/logo.svg';
+
+
+export {
+    login,
+    logo
+}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
-
+import React from 'react';
+import { login, logo } from '../../constants/images';
+import './Login.scss';
 function Login() {
   return (
-    <div>Login</div>
+    <div className='login-container'>
+        
+        <div className='l-login-content'>
+            <div>
+                <img src={logo} alt="lendsqr logo" />
+            </div>
+            <div className="login-img">
+                <img src={login} alt="for login" />
+            </div>   
+        </div>
+        <div className='r-login-content'>
+            <div className="logo">
+                <img src={logo} alt="lendsqr logo" />
+            </div>
+            <div className="login-form">
+                <div className="r-header-text">Welcome!</div>
+                <p>Enter details to login.</p>
+                <form>
+                    <div className="r-input">
+                        <input value="" placeholder="Email" />
+                    </div>
+                    <div className="r-input">
+                        <input value="" placeholder="password" />
+                        <span>show</span>
+                    </div>
+                    <div>
+                        <span>Forgot PASSWORD?</span>
+                    </div>
+                    <div className="login-btn">
+                        LOG IN
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
